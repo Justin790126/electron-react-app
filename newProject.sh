01af64c0c635f1eb7aa9a479e270028ca59f7812
justin.chang <justin.chang@quantatw.com>
2017-05-23 15:51:29 +0800

    First commit

---
+++ newProject.sh
@@ -0,0 +1,11 @@
+npm init
+    
+npm install --save-dev electron electron-reload electron-packager
+npm install --save-dev babel-core babel-loader babel-preset-es2015 babel-preset-react
+npm install --save-dev eslint eslint-loader
+npm install --save-dev webpack webpack-dev-server
+npm install --save-dev css-loader node-sass sass-loader style-loader
+    
+npm install --save react react-dom 
+npm install --save gl-react gl-react-dom
+    
