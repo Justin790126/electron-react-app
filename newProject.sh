512f3105d3988b46117f50727cf7a4401fb3889d
justin.chang <justin.chang@quantatw.com>
2017-05-24 15:42:12 +0800

    Add interaction in newProject.sh

--- newProject.sh
+++ newProject.sh
@@ -5,7 +5,20 @@ npm install --save-dev babel-core babel-loader babel-preset-es2015 babel-preset-
 npm install --save-dev eslint eslint-loader
 npm install --save-dev webpack webpack-dev-server
 npm install --save-dev css-loader node-sass sass-loader style-loader
-    
-npm install --save react react-dom 
-npm install --save gl-react gl-react-dom
+
+while true; do
+  read -p "Want React ?" yn
+  case $yn in
+    [Yy]* )
+      npm install --save react react-dom;
+      read -p "Want gl-react ?" yes
+      case $yes in
+        [Yy]* ) npm install --save gl-react gl-react-dom; break;;
+        [Nn]* ) break;;
+      esac
+      break;;
+    [Nn]* ) break;;
+  esac
+done
+
     
