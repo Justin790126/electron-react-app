4da7e217d0e03892e03eb2379dcbbe5ab6c35489
justin.chang <justin.chang@quantatw.com>
2017-05-25 10:36:02 +0800

    Add project template

--- newProject.sh
+++ newProject.sh
@@ -1,11 +1,79 @@
 npm init
+
+read -p "Do you want Electron support ? (yes/no) " yn
+case $yn in
+  [Yy]* )
+    echo "Start install Electron modules ..."
+    npm install --save-dev electron electron-reload electron-packager
+    ;;
+  * )
+    ;;
+esac
+
+echo "\n"
+
+read -p "Do you want ES6 support ? (yes/no) " yn
+case $yn in
+  [Yy]* )
+    echo "Start install ES6 build modules ..."
+    npm install --save-dev babel-core babel-loader babel-preset-es2015 babel-preset-react
+    npm install --save-dev webpack webpack-dev-server
+    npm install --save-dev css-loader node-sass sass-loader style-loader
+    ;;
+  * )
+    ;;
+esac
+
+echo "\n"
+
+read -p "Do you want Eslint support ? (yes/no) " yn
+case $yn in
+  [Yy]* )
+    echo "Start install Eslint modules ..."
+    npm install --save-dev eslint eslint-loader
     
-npm install --save-dev electron electron-reload electron-packager
-npm install --save-dev babel-core babel-loader babel-preset-es2015 babel-preset-react
-npm install --save-dev eslint eslint-loader
-npm install --save-dev webpack webpack-dev-server
-npm install --save-dev css-loader node-sass sass-loader style-loader
+    read -p "Do you want Eslint template ? (yes/no) " yn
+    case $yn in
+      [Yy]* )
+        ./node_modules/.bin/eslint eslint --init
+        ;;
+          * ) 
+        echo "\n"    
+        ;;
+    esac
+
     
-npm install --save react react-dom 
-npm install --save gl-react gl-react-dom
+        ;;
+
+      * )
+        ;;
+esac
+
     
+echo "\n"
+   
+read -p "Do you want React support ? (yes/no) " yn
+case $yn in
+  [Yy]* )
+    echo "Start install React modules ..."
+    npm install --save react react-dom 
+    echo "\n"
+
+    read -p "Do you want gl-react support ? (yes/no) " yn
+    case $yn in
+      [Yy]* )
+        echo "Start install GL React modules ..."
+        npm install --save gl-react gl-react-dom
+        ;;
+          * )
+        ;;
+
+    esac  
+
+    ;;
+
+      * )
+    ;;
+esac
+
+echo "\n"
