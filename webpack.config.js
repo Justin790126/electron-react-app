1274279dc498ef4decbd98e2a77aa163d3ed19f2
justin.chang <justin.chang@quantatw.com>
2017-05-31 09:38:48 +0800

    Prototype webpack init

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 var path = require('path');
-
+ 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
+  entry: './src/index.js', 
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
   module: {
     loaders: [
-      { 
+      {
         test: /\.css$/, loaders: ['style-loader', 'css-loader']
       },
-      { 
+      {
         test: /\.js$/,
-        exclude: /node_mudules/,
-        loader: 'babel-loader',
+        exclude: /node_modules/,
+        loaders: 'babel-loader',
         query: {
-          presets: ['es2015','react']
+          presets: ['es2015', 'react']
         }
       },
     ]
